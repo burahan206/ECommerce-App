a5c367141dfbfcbe8cb43cb2eb94cdd0a51ab943
burahan206 <141344974+burahan206@users.noreply.github.com>
2025-01-06 20:33:37 +0700

    complete cart page

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from "react";
 import { products } from "../assets/assets";
 import { toast } from "react-toastify";
+import { useNavigate } from "react-router-dom";
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search,setSearch] = useState('')
     const [showSearch,setShowSearch] = useState(false)
     const [cartItems,setCartItems] = useState({});
+    const navigate = useNavigate()
 
 
     const addToCart = async (ItemId,size) => {
@@ -52,11 +54,37 @@ const ShopContextProvider = (props) => {
         return totalCount;
     }
 
+    const updateQuantity = async (ItemId,size,quantity)=> {
+    let cartData = structuredClone(cartItems);
+
+    cartData[ItemId][size] = quantity;
+
+    setCartItems(cartData);
+    }
+
+    const getCartAmount = () => {
+        let totaAmount = 0 ;
+        for(const items in cartItems){
+            let itemInfo = products.find((product)=>product._id === items)
+            for(const item in cartItems[items]){
+                try{
+                    if (cartItems[items][item]>0) {
+                        totaAmount += itemInfo.price * cartItems[items][item]
+                    }
+                }catch(error){
+                        
+                    }
+            }
+        }
+        return totaAmount;
+    }
+
     const value = {
        products , currency, delivery_fee,
        search,setSearch,showSearch,setShowSearch,
        cartItems,addToCart,
-       getCartCount
+       getCartCount,updateQuantity,
+       getCartAmount,navigate
     }
 
     return (
