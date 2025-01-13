ec2276be546b15019c5237660dea813fac288517
burahan206 <141344974+burahan206@users.noreply.github.com>
2025-01-13 18:15:23 +0700

    connect order page backend

--- src/context/ShopContext.jsx
+++ src/context/ShopContext.jsx
@@ -138,7 +138,7 @@ const ShopContextProvider = (props) => {
     const value = {
        products , currency, delivery_fee,
        search,setSearch,showSearch,setShowSearch,
-       cartItems,addToCart,
+       cartItems,addToCart,setCartItems,
        getCartCount,updateQuantity,
        getCartAmount,navigate ,backendUrl,
        setToken,token
